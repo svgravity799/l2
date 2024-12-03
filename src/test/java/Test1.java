30c8becb98d646553addd0b3d4a9a199e053bb26
Pavel Ya. <svgravity799@gmail.com>
2024-12-03 13:19:35 +0300

    test1 added Fs perexod na karto4ky

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-
+import java.time.Duration;
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -20,13 +23,28 @@ $("[class=form__field]").setValue("testfsabonent@yandex.ru");
 $("[id=password]").setValue("Abonent13!");
 $("[type=submit]").click();
 $("[class=arrival-country-field__pinput]").click();
-        //$x("//div[text()='АОЭ']").click()
 $(byText("Турция")).click();
-     //   $("[class=calendar-popup__body_month-header-arrow-right]").click();
-     //   $("[class=calendar-popup__body_month-header-arrow-right]").click();
-    //    $("[class=calendar-popup__body_month-header-arrow-right]").click();
-     //   $("[class=calendar-popup__body_month-header-arrow-right]").click();
-     //   $("[class=calendar-popup__body_month-header-arrow-right]").click();
+      $("[class=calendar-popup__body_month-header-arrow-right]").click();  //Промотка календаря
+      $("[class=calendar-popup__body_month-header-arrow-right]").click();
+      $("[class=calendar-popup__body_month-header-arrow-right]").click();
+      $("[class=calendar-popup__body_month-header-arrow-right]").click();
+      $("[class=calendar-popup__body_month-header-arrow-right]").click();
+      $("[class=calendar-popup__body_month-header-arrow-right]").click();
+$(byText("12")).click();  //выбордаты
+$(byText("12")).click();
+$(byText("2")).click();  //выбор количества ночей
+$(byText("2")).click();
+      $("[data-testid=tours_searchblock_field_tourists_popup_Adminus]").click();  //уменьшение количества туристов
+      $("[data-testid=tours_searchblock_search_btn]").click();  //выполнение поиска
+        // WebDriver driver = new ChromeDriver();
+      //  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+      $("[class=popmechanic-close]").click(); // Закрытие всплывающего баннера
+      $("[class=search-resort-checkbox__top_label-arrow]").click(); // Выбор фильтра алания
+      $(byText("Kleopatra Aytur Suit Hotel")).click();
+    //  $("[alt=Saphir Hotel & Villas]").click();
+      $("[class=hotelFilters-charter filter]").click();
+      $("[class=offer-addToCartBtn-txt]").click();
+      $("[class=popmechanic-close]").click();
 
 
 
